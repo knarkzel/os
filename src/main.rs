2689fa866d919d2573612b58f2bef08e83ecd3dd
Knarkzel <knarkzel@gmail.com>
2024-01-23 11:41:00 +0100

    Small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    let vga_buffer = 0xB8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xB;
         }
     }
 
@@ -21,6 +19,6 @@ pub extern "C" fn _start() -> ! {
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
